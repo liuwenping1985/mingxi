fb3a8cd0dbcd92a475413e5897e82b69564ceff1
liuwenping1985 <liuwenping1985@gmail.com>
2018-12-19 14:16:11 +0800

    1

--- src/main/webapp/plugin/nbd/layui/lx.js
+++ src/main/webapp/plugin/nbd/layui/lx.js
@@ -160,7 +160,7 @@
     window.layex = layex;
     window.lx = layex;
     layex.config({
-        base: '/seeyon/apps_res/nbd/layui/apps/component/' //你存放新模块的目录，注意，不是layui的模块目录
+        base: 'layui/apps/component/' //你存放新模块的目录，注意，不是layui的模块目录
     });
 
 }());
