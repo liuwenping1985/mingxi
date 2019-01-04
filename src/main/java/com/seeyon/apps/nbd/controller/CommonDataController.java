cf9b4f671115ab423ab3d8011a7dd46890e381b9
liuwenping1985 <liuwenping1985@gmail.com>
2019-01-04 17:06:20 +0800

    1

--- src/main/java/com/seeyon/apps/nbd/controller/CommonDataController.java
+++ src/main/java/com/seeyon/apps/nbd/controller/CommonDataController.java
@@ -19,6 +19,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -56,7 +58,14 @@ public class CommonDataController extends BaseController {
         CommonParameter parameter = CommonParameter.parseParameter(request);
         String day = parameter.$("day");
         String startDay = day+" 23:59:59";
-        Date endDayDate = new Date(CommonUtils.parseDate(startDay).getTime()-24*3600*1000);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date endDayDate = null;
+        try {
+            endDayDate = new Date(sdf.parse(startDay).getTime()-24*3600*1000);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            endDayDate = new Date();
+        }
         String endDay = CommonUtils.formatDate(endDayDate);
         String sql = "select * from zrzx_user_schedule where name='S_LEADER' and start_date <='" + startDay + "' and end_date>= '" + endDay + "'";
         Collection<String> sLeaderName = ZrzxLeaderOutExportProcessor.S_LEADER.keySet();
@@ -117,55 +126,73 @@ public class CommonDataController extends BaseController {
     @NeedlessCheckLogin
     public ModelAndView getAccountLeaderCalendar(HttpServletRequest request, HttpServletResponse response) {
         CommonUtils.processCrossOriginResponse(response);
-
-        CommonParameter parameter = CommonParameter.parseParameter(request);
-        String day = parameter.$("day");
-        //day = day+" 23:59:59";
-        String startDay = day+" 23:59:59";
-        Date endDayDate = new Date(CommonUtils.parseDate(startDay).getTime()-24*3600*1000);
-        String endDay = CommonUtils.formatDate(endDayDate);
-        String sql = "select * from zrzx_user_schedule where name='M_LEADER' and start_date <='" + startDay + "' and end_date>= '" + endDay + "'";
-        Set<String> mLeaderName = ZrzxLeaderOutExportProcessor.M_LEADER.keySet();
-        NbdResponseEntity entity = new NbdResponseEntity();
         try {
-            Map<String,ZrzxUserSchedule> retMap = new HashMap<String, ZrzxUserSchedule>();
-            for(String sName:mLeaderName){
-                ZrzxUserSchedule srzx = new ZrzxUserSchedule();
-                srzx.setLocation("");
-                srzx.setReason("");
-                srzx.setUserName(sName);
-                retMap.put(sName,srzx);
+            CommonParameter parameter = CommonParameter.parseParameter(request);
+            String day = parameter.$("day");
+            //day = day+" 23:59:59";
+            String startDay = day + " 23:59:59";
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date endDayDate = null;
+            try {
+                endDayDate = new Date(sdf.parse(startDay).getTime()-24*3600*1000);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                endDayDate = new Date();
             }
-            DataLink dl = ConfigService.getA8DefaultDataLink();
-            List<ZrzxUserSchedule> mDataList = DataBaseHelper.executeObjectQueryBySQLAndLink(dl, ZrzxUserSchedule.class, sql);
-            if (!CommonUtils.isEmpty(mDataList)) {
-                for (ZrzxUserSchedule schedule : mDataList) {
-                    retMap.put(schedule.getUserName(),schedule);
+            String endDay = CommonUtils.formatDate(endDayDate);
+            String sql = "select * from zrzx_user_schedule where name='M_LEADER' and start_date <='" + startDay + "' and end_date>= '" + endDay + "'";
+            Set<String> mLeaderName = ZrzxLeaderOutExportProcessor.M_LEADER.keySet();
+            NbdResponseEntity entity = new NbdResponseEntity();
+            try {
+                Map<String, ZrzxUserSchedule> retMap = new HashMap<String, ZrzxUserSchedule>();
+                for (String sName : mLeaderName) {
+                    ZrzxUserSchedule srzx = new ZrzxUserSchedule();
+                    srzx.setLocation("");
+                    srzx.setReason("");
+                    srzx.setUserName(sName);
+                    retMap.put(sName, srzx);
                 }
-            }
-            List<ZrzxUserSchedule> vals = new ArrayList<ZrzxUserSchedule>();
-            vals.addAll(retMap.values());
-            Collections.sort(vals, new Comparator<ZrzxUserSchedule>() {
-                public int compare(ZrzxUserSchedule o1, ZrzxUserSchedule o2) {
-                    Integer ret1 = ZrzxLeaderOutExportProcessor.M_LEADER.get(o1.getUserName());
-                    if(ret1==null){
-                        ret1 = 0;
+                DataLink dl = ConfigService.getA8DefaultDataLink();
+                List<ZrzxUserSchedule> mDataList = DataBaseHelper.executeObjectQueryBySQLAndLink(dl, ZrzxUserSchedule.class, sql);
+                if (!CommonUtils.isEmpty(mDataList)) {
+                    for (ZrzxUserSchedule schedule : mDataList) {
+                        retMap.put(schedule.getUserName(), schedule);
                     }
-                    Integer ret2 = ZrzxLeaderOutExportProcessor.M_LEADER.get(o2.getUserName());
-                    if(ret2==null){
-                        ret2 = 0;
-                    }
-                    return ret1-ret2;
                 }
-            });
-            entity.setItems(vals);
-        } catch (Exception e) {
-            e.printStackTrace();
-            entity.setResult(false);
-            entity.setMsg("[ERROR]" + e.getMessage());
-        }
-        UIUtils.responseJSON(entity, response);
+                List<ZrzxUserSchedule> vals = new ArrayList<ZrzxUserSchedule>();
+                vals.addAll(retMap.values());
+                Collections.sort(vals, new Comparator<ZrzxUserSchedule>() {
+                    public int compare(ZrzxUserSchedule o1, ZrzxUserSchedule o2) {
+                        Integer ret1 = ZrzxLeaderOutExportProcessor.M_LEADER.get(o1.getUserName());
+                        if (ret1 == null) {
+                            ret1 = 0;
+                        }
+                        Integer ret2 = ZrzxLeaderOutExportProcessor.M_LEADER.get(o2.getUserName());
+                        if (ret2 == null) {
+                            ret2 = 0;
+                        }
+                        return ret1 - ret2;
+                    }
+                });
+                List<Map> dt = new ArrayList<Map>();
+                for (ZrzxUserSchedule userSchedule : vals) {
 
+                    String jsonString = JSON.toJSONString(userSchedule);
+                    Map map = JSON.parseObject(jsonString, HashMap.class);
+                    map.put("id", String.valueOf(map.get("id")));
+                    map.put("userId", String.valueOf(map.get("userId")));
+                    dt.add(map);
+                }
+                entity.setItems(dt);
+            } catch (Exception e) {
+                e.printStackTrace();
+                entity.setResult(false);
+                entity.setMsg("[ERROR]" + e.getMessage());
+            }
+            UIUtils.responseJSON(entity, response);
+        }catch(Exception e){
+            e.printStackTrace();;
+        }
         return null;
     }
 
@@ -184,14 +211,31 @@ public class CommonDataController extends BaseController {
         CommonParameter parameter = CommonParameter.parseParameter(request);
         String day = parameter.$("day");
         String startDay = day+" 23:59:59";
-        Date endDayDate = new Date(CommonUtils.parseDate(startDay).getTime()-24*3600*1000);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date endDayDate = null;
+        try {
+            endDayDate = new Date(sdf.parse(startDay).getTime()-24*3600*1000);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            endDayDate = new Date();
+        }
+       // Date endDayDate = new Date(CommonUtils.parseDate(startDay).getTime()-24*3600*1000);
         String endDay = CommonUtils.formatDate(endDayDate);
         //day = day+" 23:59:59";
         String sql = "select * from zrzx_user_schedule where user_id="+user.getId()+" and start_date <='" + startDay + "' and end_date>= '" + endDay + "'";
         DataLink dl = ConfigService.getA8DefaultDataLink();
         List<ZrzxUserSchedule> mDataList = DataBaseHelper.executeObjectQueryBySQLAndLink(dl, ZrzxUserSchedule.class, sql);
         if(!CommonUtils.isEmpty(mDataList)){
-            entity.setItems(mDataList);
+            List<Map> dt=new ArrayList<Map>();
+            for(ZrzxUserSchedule userSchedule : mDataList){
+
+                String jsonString= JSON.toJSONString(userSchedule);
+                Map map = JSON.parseObject(jsonString,HashMap.class);
+                map.put("id",String.valueOf(map.get("id")));
+                map.put("userId",String.valueOf(map.get("userId")));
+                dt.add(map);
+            }
+            entity.setItems(dt);
             entity.setResult(true);
         }else{
             entity.setData(null);
