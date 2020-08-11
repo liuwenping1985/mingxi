6b5e293674d1ecee79f429cce47edea7a80ec082
liuwenping1985 <liuwenping1985@gmail.com>
2020-08-11 17:55:31 +0800

    1

---
+++ src/main/java/com/xad/weboffice/common/CommonUtil.java
@@ -0,0 +1,16 @@
+package com.xad.weboffice.common;
+
+/**
+ * Created by liuwenping on 2020/8/11.
+ */
+public class CommonUtil {
+
+
+    public static void main(String[] args){
+
+
+
+
+
+    }
+}
