9f61858360c6b02aec54eb6282d07e97f830c65a
liuwenping1985 <liuwenping1985@gmail.com>
2020-08-16 14:44:44 +0800

    1

--- src/main/java/com/xad/weboffice/web/XadWebServerFactoryCustomizer.java
+++ src/main/java/com/xad/weboffice/web/XadWebServerFactoryCustomizer.java
@@ -1,7 +1,11 @@
 package com.xad.weboffice.web;
 
 import com.xad.weboffice.web.undertow.XadUndertowWebServerCustomizer;
+import io.undertow.Undertow.Builder;
+import org.springframework.boot.web.embedded.undertow.UndertowBuilderCustomizer;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,10 +14,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class XadWebServerFactoryCustomizer extends XadUndertowWebServerCustomizer {
 
-
     @Override
-    public void customize(UndertowServletWebServerFactory undertowServletWebServerFactory) {
+    public void customize(UndertowServletWebServerFactory undertowFactory) {
+        System.out.println("XadWebServerFactoryCustomizer started");
+    }
 
+    @Bean
+    public UndertowServletWebServerFactory servletWebServerFactory(){
+        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
+        factory.addBuilderCustomizers((Builder builder)->{builder.});
+        return factory;
     }
 
 }
